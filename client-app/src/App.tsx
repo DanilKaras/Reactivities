742c54ab395d7011550723ab418acaf838c780b1
Danylo <danilkaras@gmail.com>
2021-05-08 23:58:20 -0400

    Mediatr

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
