9668324ce9a5e932ab11cdb9934072bf3d9040a9
Danylo <danilkaras@gmail.com>
2021-05-09 22:23:37 -0400

    cruds in react

---
+++ client-app/src/features/activities/dahsboard/ActivityList.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button, Item, Label, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+    activities: Activity[];
+    selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
+}
+const ActivityList = ({activities, selectActivity, deleteActivity}: Props) => {
+    return (
+        <Segment>
+            <Item.Group divided>
+                {activities.map(item => 
+                    <Item key={item.id}>
+                        <Item.Content>
+                            <Item.Header as='a'>{item.title}</Item.Header>
+                            <Item.Meta>{item.date}</Item.Meta>
+                            <Item.Description>
+                                <div>{item.description}</div>
+                                <div>{item.city}, {item.venue}</div>
+                            </Item.Description>
+                            <Item.Extra>
+                                <Button onClick={() => selectActivity(item.id)} floated='right' content='View' color='blue'/>
+                                <Button onClick={() => deleteActivity(item.id)} floated='right' content='Delete' color='red'/>
+                                <Label  basic content={item.category}/>
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                )}
+            </Item.Group>
+        </Segment>
+    )
+}
+
+export default ActivityList;
