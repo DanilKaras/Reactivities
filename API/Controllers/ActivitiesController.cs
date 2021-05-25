1ddaaa461160b24ede52773b0f057731c2f54f7f
Danylo <danilkaras@gmail.com>
2021-05-24 20:56:37 -0400

    Attendees

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [AllowAnonymous]
     public class ActivitiesController : BaseApiController
     {
         [HttpGet]
@@ -32,7 +31,7 @@ namespace API.Controllers
             return HandleResult(result);
         }
 
-
+        [Authorize(Policy = "IsActivityHost")]
         [HttpPut("{id}")]
         public async Task<IActionResult> EditActivity(Guid id, [FromBody]Activity activity)
         {
@@ -40,11 +39,20 @@ namespace API.Controllers
             var result = await Mediator.Send(new Edit.Command{Activity = activity});
             return HandleResult(result);
         }
-
+        
+        [Authorize(Policy = "IsActivityHost")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteActivity(Guid id)
         {       
             var result = await Mediator.Send(new Delete.Command{Id = id});
+            return HandleResult(result);
+        }
+
+
+        [HttpPost("{id}/attend")]
+        public async Task<IActionResult> Attend(Guid id)
+        {
+            var result = await Mediator.Send(new UpdateAttendance.Command{Id = id});
             return HandleResult(result);
         }
     }
