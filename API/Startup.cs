c8767276f5b7551d0b195cdfba948bfef654b762
Danylo <danilkaras@gmail.com>
2021-08-08 22:57:42 -0400

    Heroku deploy

--- API/Startup.cs
+++ API/Startup.cs
@@ -65,6 +65,10 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseCors("CorsPolicy");
 
             app.UseAuthentication();
@@ -75,6 +79,7 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<ChatHub>("/chat");
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
